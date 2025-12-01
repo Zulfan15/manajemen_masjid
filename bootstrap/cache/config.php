e4b2ae6285a698eea25fecd18c1eaf2a7dabbaf6
x3merx <indox3m@gmail.com>
2025-12-01 09:51:48 +0700

    Feat(new): New Branch

---
+++ bootstrap/cache/config.php
@@ -0,0 +1,468 @@
+<?php return array (
+  'app' => 
+  array (
+    'name' => 'Manajemen Masjid',
+    'env' => 'local',
+    'debug' => true,
+    'url' => 'http://localhost',
+    'asset_url' => NULL,
+    'timezone' => 'Asia/Jakarta',
+    'locale' => 'id',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'id_ID',
+    'key' => 'base64:St/re058uYOthU45HyCQGHgFZa6ajS540SvIS4U1BR0=',
+    'cipher' => 'AES-256-CBC',
+    'maintenance' => 
+    array (
+      'driver' => 'file',
+    ),
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Auth\\AuthServiceProvider',
+      1 => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+      2 => 'Illuminate\\Bus\\BusServiceProvider',
+      3 => 'Illuminate\\Cache\\CacheServiceProvider',
+      4 => 'Illuminate\\Foundation\\Providers\\ConsoleSupportServiceProvider',
+      5 => 'Illuminate\\Cookie\\CookieServiceProvider',
+      6 => 'Illuminate\\Database\\DatabaseServiceProvider',
+      7 => 'Illuminate\\Encryption\\EncryptionServiceProvider',
+      8 => 'Illuminate\\Filesystem\\FilesystemServiceProvider',
+      9 => 'Illuminate\\Foundation\\Providers\\FoundationServiceProvider',
+      10 => 'Illuminate\\Hashing\\HashServiceProvider',
+      11 => 'Illuminate\\Mail\\MailServiceProvider',
+      12 => 'Illuminate\\Notifications\\NotificationServiceProvider',
+      13 => 'Illuminate\\Pagination\\PaginationServiceProvider',
+      14 => 'Illuminate\\Auth\\Passwords\\PasswordResetServiceProvider',
+      15 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+      16 => 'Illuminate\\Queue\\QueueServiceProvider',
+      17 => 'Illuminate\\Redis\\RedisServiceProvider',
+      18 => 'Illuminate\\Session\\SessionServiceProvider',
+      19 => 'Illuminate\\Translation\\TranslationServiceProvider',
+      20 => 'Illuminate\\Validation\\ValidationServiceProvider',
+      21 => 'Illuminate\\View\\ViewServiceProvider',
+      22 => 'Spatie\\Permission\\PermissionServiceProvider',
+      23 => 'Spatie\\Activitylog\\ActivitylogServiceProvider',
+      24 => 'App\\Providers\\AppServiceProvider',
+      25 => 'App\\Providers\\AuthServiceProvider',
+      26 => 'App\\Providers\\EventServiceProvider',
+      27 => 'App\\Providers\\RouteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+  ),
+  'auth' => 
+  array (
+    'defaults' => 
+    array (
+      'guard' => 'web',
+      'passwords' => 'users',
+    ),
+    'guards' => 
+    array (
+      'web' => 
+      array (
+        'driver' => 'session',
+        'provider' => 'users',
+      ),
+    ),
+    'providers' => 
+    array (
+      'users' => 
+      array (
+        'driver' => 'eloquent',
+        'model' => 'App\\Models\\User',
+      ),
+    ),
+    'passwords' => 
+    array (
+      'users' => 
+      array (
+        'provider' => 'users',
+        'table' => 'password_reset_tokens',
+        'expire' => 60,
+        'throttle' => 60,
+      ),
+    ),
+    'password_timeout' => 10800,
+  ),
+  'cache' => 
+  array (
+    'default' => 'file',
+    'stores' => 
+    array (
+      'apc' => 
+      array (
+        'driver' => 'apc',
+      ),
+      'array' => 
+      array (
+        'driver' => 'array',
+        'serialize' => false,
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'table' => 'cache',
+        'connection' => NULL,
+        'lock_connection' => NULL,
+      ),
+      'file' => 
+      array (
+        'driver' => 'file',
+        'path' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\framework/cache/data',
+      ),
+      'memcached' => 
+      array (
+        'driver' => 'memcached',
+        'persistent_id' => NULL,
+        'sasl' => 
+        array (
+          0 => NULL,
+          1 => NULL,
+        ),
+        'options' => 
+        array (
+        ),
+        'servers' => 
+        array (
+          0 => 
+          array (
+            'host' => '127.0.0.1',
+            'port' => 11211,
+            'weight' => 100,
+          ),
+        ),
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'cache',
+        'lock_connection' => 'default',
+      ),
+      'dynamodb' => 
+      array (
+        'driver' => 'dynamodb',
+        'key' => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+        'table' => 'cache',
+        'endpoint' => NULL,
+      ),
+      'octane' => 
+      array (
+        'driver' => 'octane',
+      ),
+    ),
+    'prefix' => 'laravel_cache_',
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'sqlite' => 
+      array (
+        'driver' => 'sqlite',
+        'url' => NULL,
+        'database' => 'manajemen_masjid',
+        'prefix' => '',
+        'foreign_key_constraints' => true,
+      ),
+      'mysql' => 
+      array (
+        'driver' => 'mysql',
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'manajemen_masjid',
+        'username' => 'root',
+        'password' => 'root',
+        'unix_socket' => '',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => NULL,
+        'options' => 
+        array (
+        ),
+      ),
+      'pgsql' => 
+      array (
+        'driver' => 'pgsql',
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'manajemen_masjid',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'search_path' => 'public',
+        'sslmode' => 'prefer',
+      ),
+      'sqlsrv' => 
+      array (
+        'driver' => 'sqlsrv',
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'database' => 'manajemen_masjid',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        'prefix' => '',
+        'prefix_indexes' => true,
+      ),
+    ),
+    'migrations' => 'migrations',
+    'redis' => 
+    array (
+      'client' => 'phpredis',
+      'options' => 
+      array (
+        'cluster' => 'redis',
+        'prefix' => 'manajemen_masjid_database_',
+      ),
+      'default' => 
+      array (
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'username' => NULL,
+        'password' => NULL,
+        'port' => '6379',
+        'database' => '0',
+      ),
+      'cache' => 
+      array (
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'username' => NULL,
+        'password' => NULL,
+        'port' => '6379',
+        'database' => '1',
+      ),
+    ),
+  ),
+  'filesystems' => 
+  array (
+    'default' => 'local',
+    'disks' => 
+    array (
+      'local' => 
+      array (
+        'driver' => 'local',
+        'root' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\app',
+        'throw' => false,
+      ),
+      'public' => 
+      array (
+        'driver' => 'local',
+        'root' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\app/public',
+        'url' => 'http://localhost/storage',
+        'visibility' => 'public',
+        'throw' => false,
+      ),
+      's3' => 
+      array (
+        'driver' => 's3',
+        'key' => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+        'bucket' => '',
+        'url' => NULL,
+        'endpoint' => NULL,
+        'use_path_style_endpoint' => false,
+        'throw' => false,
+      ),
+    ),
+    'links' => 
+    array (
+      'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\public\\storage' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\app/public',
+    ),
+  ),
+  'logging' => 
+  array (
+    'default' => 'stack',
+    'deprecations' => 
+    array (
+      'channel' => NULL,
+      'trace' => false,
+    ),
+    'channels' => 
+    array (
+      'stack' => 
+      array (
+        'driver' => 'stack',
+        'channels' => 
+        array (
+          0 => 'single',
+        ),
+        'ignore_exceptions' => false,
+      ),
+      'single' => 
+      array (
+        'driver' => 'single',
+        'path' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\logs/laravel.log',
+        'level' => 'debug',
+        'replace_placeholders' => true,
+      ),
+      'daily' => 
+      array (
+        'driver' => 'daily',
+        'path' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\logs/laravel.log',
+        'level' => 'debug',
+        'days' => 14,
+        'replace_placeholders' => true,
+      ),
+      'slack' => 
+      array (
+        'driver' => 'slack',
+        'url' => NULL,
+        'username' => 'Laravel Log',
+        'emoji' => ':boom:',
+        'level' => 'debug',
+        'replace_placeholders' => true,
+      ),
+      'papertrail' => 
+      array (
+        'driver' => 'monolog',
+        'level' => 'debug',
+        'handler' => 'Monolog\\Handler\\SyslogUdpHandler',
+        'handler_with' => 
+        array (
+          'host' => NULL,
+          'port' => NULL,
+          'connectionString' => 'tls://:',
+        ),
+        'processors' => 
+        array (
+          0 => 'Monolog\\Processor\\PsrLogMessageProcessor',
+        ),
+      ),
+      'stderr' => 
+      array (
+        'driver' => 'monolog',
+        'level' => 'debug',
+        'handler' => 'Monolog\\Handler\\StreamHandler',
+        'formatter' => NULL,
+        'with' => 
+        array (
+          'stream' => 'php://stderr',
+        ),
+        'processors' => 
+        array (
+          0 => 'Monolog\\Processor\\PsrLogMessageProcessor',
+        ),
+      ),
+      'syslog' => 
+      array (
+        'driver' => 'syslog',
+        'level' => 'debug',
+        'facility' => 8,
+        'replace_placeholders' => true,
+      ),
+      'errorlog' => 
+      array (
+        'driver' => 'errorlog',
+        'level' => 'debug',
+        'replace_placeholders' => true,
+      ),
+      'null' => 
+      array (
+        'driver' => 'monolog',
+        'handler' => 'Monolog\\Handler\\NullHandler',
+      ),
+      'emergency' => 
+      array (
+        'path' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\logs/laravel.log',
+      ),
+    ),
+  ),
+  'permission' => 
+  array (
+    'models' => 
+    array (
+      'permission' => 'Spatie\\Permission\\Models\\Permission',
+      'role' => 'Spatie\\Permission\\Models\\Role',
+    ),
+    'table_names' => 
+    array (
+      'roles' => 'roles',
+      'permissions' => 'permissions',
+      'model_has_permissions' => 'model_has_permissions',
+      'model_has_roles' => 'model_has_roles',
+      'role_has_permissions' => 'role_has_permissions',
+    ),
+    'column_names' => 
+    array (
+      'role_pivot_key' => NULL,
+      'permission_pivot_key' => NULL,
+      'model_morph_key' => 'model_id',
+      'team_foreign_key' => 'team_id',
+    ),
+    'register_permission_check_method' => true,
+    'register_octane_reset_listener' => false,
+    'events_enabled' => false,
+    'teams' => false,
+    'team_resolver' => 'Spatie\\Permission\\DefaultTeamResolver',
+    'use_passport_client_credentials' => false,
+    'display_permission_in_exception' => false,
+    'display_role_in_exception' => false,
+    'enable_wildcard_permission' => false,
+    'cache' => 
+    array (
+      'expiration_time' => 
+      \DateInterval::__set_state(array(
+         'from_string' => true,
+         'date_string' => '24 hours',
+      )),
+      'key' => 'spatie.permission.cache',
+      'store' => 'default',
+    ),
+  ),
+  'session' => 
+  array (
+    'driver' => 'file',
+    'lifetime' => '120',
+    'expire_on_close' => false,
+    'encrypt' => false,
+    'files' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\framework/sessions',
+    'connection' => NULL,
+    'table' => 'sessions',
+    'store' => NULL,
+    'lottery' => 
+    array (
+      0 => 2,
+      1 => 100,
+    ),
+    'cookie' => 'manajemen_masjid_session',
+    'path' => '/',
+    'domain' => NULL,
+    'secure' => NULL,
+    'http_only' => true,
+    'same_site' => 'lax',
+  ),
+  'view' => 
+  array (
+    'paths' => 
+    array (
+      0 => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\resources\\views',
+    ),
+    'compiled' => 'D:\\Backup\\tahun ajaran 4-1 smester 7\\ManPro\\Manajemen Masjid\\manajemen_masjid\\storage\\framework\\views',
+  ),
+  'activitylog' => 
+  array (
+    'enabled' => true,
+    'delete_records_older_than_days' => 365,
+    'default_log_name' => 'default',
+    'default_auth_driver' => NULL,
+    'subject_returns_soft_deleted_models' => false,
+    'activity_model' => 'Spatie\\Activitylog\\Models\\Activity',
+    'table_name' => 'activity_log',
+    'database_connection' => NULL,
+  ),
+);
---
+++ storage/framework/views/219d84fb6f7e527e955a47a10d0c7cba.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Backup\tahun ajaran 4-1 smester 7\ManPro\Manajemen Masjid\manajemen_masjid\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
