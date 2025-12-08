275788cf16dfa5b98a8f2890b76e4325dbfa45f2
x3merx <indox3m@gmail.com>
2025-12-08 07:52:38 +0700

    feat(JadwalPerawatan): Creat JadwalPerawatan model

--- app/Models/JadwalPerawatan.php
+++ app/Models/JadwalPerawatan.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class JadwalPerawatan extends Model
 {
     use HasFactory;
+
+    protected $table = 'jadwal_perawatan';
+    protected $primaryKey = 'jadwal_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'aset_id',
+        'tanggal_jadwal',
+        'jenis_perawatan',
+        'status',
+        'id_petugas',
+        'note',
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo(Aset::class, 'aset_id', 'aset_id');
+    }
+
+    public function petugas()
+    {
+        return $this->belongsTo(User::class, 'id_petugas', 'id');
+    }
 }
