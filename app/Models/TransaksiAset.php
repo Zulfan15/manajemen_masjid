9034a21538a027dae849851b9255bee446e54769
x3merx <indox3m@gmail.com>
2025-12-08 07:49:41 +0700

    feat(TransaksiAset): Create TransaksiAset model

--- app/Models/TransaksiAset.php
+++ app/Models/TransaksiAset.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class TransaksiAset extends Model
 {
     use HasFactory;
+
+    protected $table = 'add_min_aset';
+    protected $primaryKey = 'transaksi_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'aset_id',
+        'jenis_transaksi',
+        'tanggal_transaksi',
+        'jumlah',
+        'id_petugas',
+        'keterangan',
+    ];
+
+    public function aset()
+    {
+        return $this->belongsTo(Aset::class, 'aset_id', 'aset_id');
+    }
+
+    public function petugas()
+    {
+        return $this->belongsTo(User::class, 'id_petugas', 'id');
+    }
 }
